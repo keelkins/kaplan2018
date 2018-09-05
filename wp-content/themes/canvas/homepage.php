4042e406f4ff785daa04c3f708998833c5820c32
Unknown <kira@neoncanvas.com>
2018-09-05 14:07:23 -0400

    Website Homepage  Build

--- wp-content/themes/canvas/homepage.php
+++ wp-content/themes/canvas/homepage.php
@@ -15,7 +15,7 @@ get_header(); ?>
   		<div class="bgvideo">
   			<video width="100%" height="100%" autoplay muted loop playsinline preload="metadata">
             <source src="/wp-content/themes/canvas/img/Header.webmhd.webm" type="video/webm">
-            <source src="/wp-content/themes/canvas/img/Header.mp4" type="video/mp4">
+            <source src="/wp-content/themes/canvas/img/Header.mp4.mp4" type="video/mp4">
             <source src="/wp-content/themes/canvas/img/Header.oggtheora.ogv" type="video/ogg">
   			</video>
   		</div>

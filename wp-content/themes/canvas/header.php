9814df52f74ebe586ef9b62ac65f232b00a6f730
Nelson <nelson@neoncanvas.com>
2019-11-01 10:27:54 -0400

    Remove chat script

--- wp-content/themes/canvas/header.php
+++ wp-content/themes/canvas/header.php
@@ -30,18 +30,6 @@
 	  gtag('config', 'UA-99002712-1');
 	</script>
 
-	<!-- Ortho Chat Script -->
-	<script type="text/javascript">
-	  var __lc = {};
-	  __lc.license = 7036871;
-
-	  (function() {
-	    var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
-	    lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
-	  })();
-	</script>
-
 	<!-- Animate.css -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
 
